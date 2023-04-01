b1fb36e8690cb4c750d93a8fcb8b72593ce38af2
Dustin L <r.dustin.lane@gmail.com>
2023-04-01 16:06:16 -0700

    magtag CO2 updates

--- ports/espressif/boards/adafruit_magtag_2.9_grayscale/mpconfigboard.mk
+++ ports/espressif/boards/adafruit_magtag_2.9_grayscale/mpconfigboard.mk
@@ -11,10 +11,18 @@ CIRCUITPY_ESP_FLASH_SIZE = 4MB
 
 CIRCUITPY_ESPCAMERA = 0
 
+# Enable micropython.native
+CIRCUITPY_ENABLE_MPY_NATIVE = 1
+
 # Include these Python libraries in firmware.
 FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_PortalBase
-FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_FakeRequests
-FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_Requests
+#FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_FakeRequests
+#FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_Requests
 FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_NeoPixel
 FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_Display_Text
 FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_LIS3DH
+FROZEN_MPY_DIRS += $(TOP)/frozen/CircuitPython_HomeAssistant
+FROZEN_MPY_DIRS += $(TOP)/frozen/Magtag_CO2
+#FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_MagTag
+FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_MiniMQTT
+#FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_Bitmap_Font
