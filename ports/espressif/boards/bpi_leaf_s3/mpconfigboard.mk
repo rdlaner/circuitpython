7d8320298c59cc04f0ab6555940410ac2631e9c8
Dustin L <r.dustin.lane@gmail.com>
2023-05-12 20:47:32 -0700

    BPI Leaf S3 - Add frozen libraries to BPI Leaf S3 build

--- ports/espressif/boards/bpi_leaf_s3/mpconfigboard.mk
+++ ports/espressif/boards/bpi_leaf_s3/mpconfigboard.mk
@@ -14,3 +14,13 @@ CIRCUITPY_ENABLE_MPY_NATIVE = 1
 
 # Include these Python libraries in firmware.
 FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_NeoPixel
+FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_Logging
+FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_MiniMQTT
+FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_asyncio
+FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_Ticks
+FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_NTP
+FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_SCD4X
+FROZEN_MPY_DIRS += $(TOP)/frozen/CircuitPython_Libs
+FROZEN_MPY_DIRS += $(TOP)/frozen/CircuitPython_HomeAssistant
+FROZEN_MPY_DIRS += $(TOP)/frozen/ESPNow_Gateway
+FROZEN_MPY_DIRS += $(TOP)/frozen/BPI_Leaf_CO2
